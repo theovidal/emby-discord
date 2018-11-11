5815961fb8c92ff42fd8222ca48ea4568553a840
Exybore <exybore@gmail.com>
2018-11-11 15:38:43 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,109 @@
+'use strict'
+const { app, BrowserWindow } = require('electron')
+const RPC = require('discord-rpc')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    resizable: true,
+    useContentSize: false,
+    autoHideMenuBar: true,
+    center: true,
+    titleBarStyle: 'hidden',
+    frame: true,
+    alwaysOnTop: false,
+    title: 'Emby',
+    webPreferences: {
+      nodeIntegration: false,
+      plugins: true,
+    }
+  })
+
+  mainWindow.loadURL('https://emby.becauseofprog.fr'
+  )
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+const clientId = '497839419089223680'
+const rpc = new RPC.Client({ transport: 'ipc' })
+const startTimestamp = new Date()
+
+async function setActivity() {
+  if (!rpc || !mainWindow) {
+    return
+  }
+
+  const info = await mainWindow.webContents.executeJavaScript(`
+    (function() {
+      var bar = document.querySelector('.nowPlayingBar')
+      if (bar === null) {
+        return {
+          'title': 'Browsing catalog', 
+          'subtitle': '...',
+          'icon': 'pause'
+        }
+      }
+      else {
+        if (bar.classList.contains('nowPlayingBar-hidden')) {
+          return {
+            'title': 'Switching music', 
+            'subtitle': '...',
+            'icon': 'pause'
+           }
+        }
+        else {
+          var infos = {
+            'title': document.querySelector('.nowPlayingBarText > div button.itemAction.textActionButton').innerHTML, 
+            'subtitle': document.querySelector('.nowPlayingBarSecondaryText > button.itemAction.textActionButton').innerHTML,
+            'icon': ''
+          }
+          if (document.querySelector('.playPauseButton i.md-icon').innerHTML = 'pause') {
+            infos.icon = 'play'
+          }
+          else {
+            infos.icon = 'pause'
+          }
+          return infos
+        }
+      }
+    })()
+  `)
+
+  rpc.setActivity({
+    details: info.title,
+    state: info.subtitle,
+    startTimestamp,
+    largeImageKey: 'logo',
+    largeImageText: 'Emby',
+    smallImageKey: info.icon,
+    smallImageText: info.icon === 'play' ? 'Playing' : 'Paused',
+    instance: false
+  })
+}
+
+rpc.on('ready', () => {
+  setActivity()
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity()
+  }, 5E2)
+})
+
+rpc.login({ clientId }).catch(console.error)
